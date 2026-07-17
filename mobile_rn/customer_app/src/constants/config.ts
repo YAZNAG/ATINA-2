05ae9f0c6ea44d825c74576aa856fb1e0238ddf2
YAZNAG <aznagy09@gmail.com>
2026-07-17 11:33:44 +0100

    correct image url

--- mobile_rn/customer_app/src/constants/config.ts
+++ mobile_rn/customer_app/src/constants/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  API_URL:     process.env.EXPO_PUBLIC_API_URL     ?? 'http://192.168.1.17:5000/api',
-  STORAGE_URL: process.env.EXPO_PUBLIC_STORAGE_URL ?? 'http://192.168.1.17:5000',
+  API_URL:     process.env.EXPO_PUBLIC_API_URL     ?? 'http://192.168.100.114:5000/api',
+  STORAGE_URL: process.env.EXPO_PUBLIC_STORAGE_URL ?? 'http://192.168.100.114:5000',
   TIMEOUT: 10000,
 };
